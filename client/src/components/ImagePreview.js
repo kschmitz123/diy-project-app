7a2f71c9907ef362d851a53fde1da072f71556a8
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-11-24 15:30:09 +0100

    Add ImagePreview

---
+++ client/src/components/ImagePreview.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components/macro";
+import Test from "../assets/test.jpg";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+
+const Container = styled.div`
+  width: 100%;
+  height: auto;
+  position: relative;
+`;
+const Image = styled.img`
+  border-radius: 25px;
+  width: 100%;
+  height: 100%;
+`;
+const FaveIcon = styled.button`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  background: none;
+  border: none;
+  color: var(--logo-color);
+  cursor: pointer;
+`;
+
+export const ImagePreview = () => {
+  return (
+    <Container>
+      <Image src={Test} alt="test" />
+      <FaveIcon>
+        <FavoriteIcon fontSize="large" />
+      </FaveIcon>
+    </Container>
+  );
+};
---
+++ client/src/components/ImagePreview.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { ImagePreview } from "./ImagePreview";
+
+export default {
+  title: "Component/ImagePreview",
+  component: ImagePreview,
+};
+
+export const Standard = () => <ImagePreview />;
