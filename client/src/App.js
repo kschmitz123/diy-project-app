d8e71cc855dc963e1b1d9e0c6c75734a7b7e1c85
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-11-25 17:41:02 +0100

    Set loading page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import GlobalStyle from "./GlobalStyle";
+import { LoadingScreen } from "./components/LoadingScreen";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 4000);
+  }, []);
+
   return (
     <>
       <GlobalStyle />
-      <div className="App">Test deployment</div>
+      {loading === false ? (
+        <div className="App">Test deployment</div>
+      ) : (
+        <LoadingScreen />
+      )}
     </>
   );
 }
