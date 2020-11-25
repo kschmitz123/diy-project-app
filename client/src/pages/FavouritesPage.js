f18151b1b5a62c457edeeea8e930c80ba22910b4
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-11-25 14:13:37 +0100

    Add favourites page

---
+++ client/src/pages/FavouritesPage.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components/macro";
+import { Header } from "../components/Header";
+import { ImagePreview } from "../components/ImagePreview";
+import { Navbar } from "../components/Navbar";
+
+const Container = styled.div`
+  padding: 60px 0;
+`;
+
+export const FavouritesPage = () => {
+  return (
+    <>
+      <Header title={"Favourites"} />
+      <Container>
+        <ImagePreview />
+        <ImagePreview />
+        <ImagePreview />
+        <ImagePreview />
+      </Container>
+
+      <Navbar />
+    </>
+  );
+};
