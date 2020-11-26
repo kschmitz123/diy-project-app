4aa68dc5006512e914fd89cb9efae9c71350d6a4
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-11-26 11:01:18 +0100

    Remove unused code

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -4,17 +4,6 @@ import { Categories } from "../components/Categories";
 import { ScrollMenu } from "../components/ScrollMenu";
 import { Navbar } from "../components/Navbar";
 
-// const SearchButton = styled.button`
-//   background: none;
-//   border: none;
-//   color: var(--button-color);
-//   cursor: pointer;
-//   z-index: 2;
-//   position: fixed;
-//   top: 8px;
-//   right: 10px;
-// `;
-
 export const HomePage = () => {
   return (
     <>
