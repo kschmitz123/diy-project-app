73d52ca29340e22520b8649757b886373b66578a
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-11-27 16:16:04 +0100

    Fix deployment error

--- client/src/stories/Input.stories.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Input } from "../components/Input";
-
-export default {
-  title: "Component/Input",
-  component: Input,
-};
-
-export const Standard = () => <Input />;
