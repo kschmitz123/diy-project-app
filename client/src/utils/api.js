11720dc4673f7306f6070d3c210eae1c3ef8a620
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-11-30 14:58:58 +0100

    Add getProjectbyId

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -7,3 +7,9 @@ export async function postProject(project) {
   const newProject = await response.json();
   return newProject;
 }
+
+export async function getProjectById(id) {
+  const response = await fetch(`http://localhost:5000/projects/${id}`);
+  const project = await response.json();
+  return project;
+}
