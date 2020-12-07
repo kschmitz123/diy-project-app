3e00db49b91e5d6920faeafad5247e4e7f92d86a
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-12-07 11:57:07 +0100

    Add context provider

---
+++ client/src/contexts/context.js
@@ -0,0 +1,38 @@
+import { AuthReducer, initialState } from "./reducer";
+
+import React, { useReducer } from "react";
+import PropTypes from "prop-types";
+
+const AuthStateContext = React.createContext();
+const AuthDispatchContext = React.createContext();
+
+export const useAuthState = () => {
+  const context = React.useContext(AuthStateContext);
+  if (context === undefined) {
+    throw new Error("useAuthState must be used within a AuthProvider");
+  }
+  return context;
+};
+
+export const useAuthDispatch = () => {
+  const context = React.useContext(AuthDispatchContext);
+  if (context === undefined) {
+    throw new Error("useAuthDispatch must be used within a AuthProvider");
+  }
+  return context;
+};
+
+export const AuthProvider = ({ children }) => {
+  const [user, dispatch] = useReducer(AuthReducer, initialState);
+  return (
+    <AuthStateContext.Provider value={user}>
+      <AuthDispatchContext.Provider value={dispatch}>
+        {children}
+      </AuthDispatchContext.Provider>
+    </AuthStateContext.Provider>
+  );
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.node,
+};
