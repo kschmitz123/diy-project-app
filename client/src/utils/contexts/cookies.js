c9bd62ee0ea9a1256298f20af3b5be68ca2baf5a
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-12-14 13:05:47 +0100

    Add cookie function

---
+++ client/src/utils/contexts/cookies.js
@@ -0,0 +1,14 @@
+import Cookies from "js-cookie";
+
+export const setSessionCookie = (username) => {
+  Cookies.set("session", username, { expires: 1 });
+};
+
+export const getSessionCookie = () => {
+  const sessionCookie = Cookies.get("session");
+  if (sessionCookie === undefined) {
+    return {};
+  } else {
+    return JSON.parse(sessionCookie);
+  }
+};
