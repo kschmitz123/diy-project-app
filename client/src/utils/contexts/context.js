3f9f2118331e4c981f7fe9a88a63077f740a957e
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-12-14 13:06:14 +0100

    Implement cookies in context

--- client/src/utils/contexts/context.js
+++ client/src/utils/contexts/context.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
+import { getSessionCookie, setSessionCookie } from "./cookies";
+import Cookies from "js-cookie";
 
 const UserContext = React.createContext();
 
@@ -12,20 +14,20 @@ export const useUserState = () => {
 };
 
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState({ username: "", auth: true });
+  const [user, setUser] = useState(getSessionCookie());
 
   const login = (username) => {
     setUser(() => ({
       username: username,
-      auth: true,
     }));
   };
+  setSessionCookie(user);
+
   const logout = () => {
-    setUser(() => ({
-      username: "",
-      auth: false,
-    }));
+    setUser(() => ({ username: "" }));
+    Cookies.remove("session");
   };
+
   return (
     <UserContext.Provider value={{ user, login, logout }}>
       {children}
