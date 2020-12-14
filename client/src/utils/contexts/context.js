e5d436a7692de8102514edb094882b0794ab454c
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-12-14 13:48:51 +0100

    Remove cookie on logout

--- client/src/utils/contexts/context.js
+++ client/src/utils/contexts/context.js
@@ -24,7 +24,6 @@ export const UserProvider = ({ children }) => {
   setSessionCookie(user);
 
   const logout = () => {
-    setUser(() => ({ username: "" }));
     Cookies.remove("session");
   };
 
