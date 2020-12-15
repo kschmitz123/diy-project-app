fc2196d92f642f5f5f935a649f345dc4454a4a9e
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-12-15 13:42:10 +0100

    Change error component

--- client/src/components/ErrorMessage.js
+++ client/src/components/ErrorMessage.js
@@ -1,15 +1,21 @@
 import styled from "styled-components/macro";
 import errorIcon from "../assets/shrug-emoticon.svg";
+import PropTypes from "prop-types";
 
 const Container = styled.div`
   text-align: center;
+  margin-top: 30px;
 `;
 
-export const ErrorMessage = () => {
+export const ErrorMessage = ({ title }) => {
   return (
     <Container>
-      <h3>Seems like this project doesn&apos;t exist anymore.</h3>
+      <h3>{title}</h3>
       <img src={errorIcon} alt="" />
     </Container>
   );
+};
+
+ErrorMessage.propTypes = {
+  title: PropTypes.string,
 };
