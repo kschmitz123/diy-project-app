5b20bcd3f74e6181ce2a16c16f401ecac565148e
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-12-15 13:50:15 +0100

    Add error message to favorites

--- client/src/pages/FavoritesPage.js
+++ client/src/pages/FavoritesPage.js
@@ -7,6 +7,7 @@ import { useQuery } from "react-query";
 import { getFavoritesByUser } from "../utils/api/users";
 import { useUserState } from "../utils/contexts/context";
 import TitlePreview from "../components/TitlePreview";
+import { ErrorMessage } from "../components/ErrorMessage";
 
 export const FavoritesPage = () => {
   const { user } = useUserState();
@@ -23,7 +24,7 @@ export const FavoritesPage = () => {
         {status === "error" && <div>404 Error fetching proejcts</div>}
         {status === "success" && (
           <span>
-            {data.favorites ? (
+            {data.favorites && data.favorites.length < 0 ? (
               data.favorites.map((favorite) => (
                 <Link key={favorite.id} to={`/projects/${favorite.id}`}>
                   <ImagePreview
@@ -35,7 +36,9 @@ export const FavoritesPage = () => {
                 </Link>
               ))
             ) : (
-              <div>Looks like you don&apos;t have any favorites yet.</div>
+              <ErrorMessage
+                title={"Looks like you don't have any favorites yet."}
+              />
             )}
           </span>
         )}
