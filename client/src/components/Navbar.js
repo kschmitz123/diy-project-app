3a1066c9ba6bed6e79ecd70de94ec54af767ced7
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-12-18 14:26:54 +0100

    Add link in navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,7 @@
 import HomeIcon from "@material-ui/icons/Home";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import AddIcon from "@material-ui/icons/Add";
+import PersonIcon from "@material-ui/icons/Person";
 import styled from "styled-components/macro";
 import { Link, useLocation } from "react-router-dom";
 
@@ -51,6 +52,13 @@ const Navbar = () => {
         active={location.pathname === "/add"}
       >
         <AddIcon fontSize="large" />
+      </NavLink>
+      <NavLink
+        aria-label={"Profile"}
+        to={"/profile"}
+        active={location.pathname === "/profile"}
+      >
+        <PersonIcon fontSize="large" />
       </NavLink>
     </Footer>
   );
