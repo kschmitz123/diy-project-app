353dfb609a0e11c6d70cb13deb3f23a6a41e8f19
kschmitz123 <kathrin_schmitz93@gmx.de>
2021-01-03 11:13:13 +0100

    Add upload button

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,11 +1,23 @@
 import styled from "styled-components/macro";
 import Avatar from "../assets/avatar-placeholder.jpeg";
 import PropTypes from "prop-types";
+import AddAPhotoIcon from "@material-ui/icons/AddAPhoto";
+import { Button } from "./Buttons";
+
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   margin-bottom: 20px;
+
+  h2 {
+    color: var(--main-color);
+    margin: 0;
+  }
+
+  div {
+    position: relative;
+  }
 
   img {
     height: 150px;
@@ -13,9 +25,13 @@ const Container = styled.div`
     border-radius: 50%;
   }
 
-  h2 {
-    color: var(--main-color);
+  button {
+    position: absolute;
+    top: 120px;
+    right: 0;
     margin: 0;
+    padding: 5px 10px;
+    border-radius: 25px;
   }
 `;
 
@@ -23,7 +39,12 @@ const Profile = ({ user }) => {
   return (
     <Container>
       <h2>{user}</h2>
-      <img src={Avatar} alt="avatar" />
+      <div>
+        <img src={Avatar} alt="avatar" />
+        <Button>
+          <AddAPhotoIcon />
+        </Button>
+      </div>
     </Container>
   );
 };
