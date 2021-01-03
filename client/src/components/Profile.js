a846a9fcd6f6edd28b11d706ce203a28bb781133
kschmitz123 <kathrin_schmitz93@gmx.de>
2021-01-03 11:31:06 +0100

    Add handleImageChange

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -3,6 +3,7 @@ import Avatar from "../assets/avatar-placeholder.jpeg";
 import PropTypes from "prop-types";
 import AddAPhotoIcon from "@material-ui/icons/AddAPhoto";
 import { Button } from "./Buttons";
+import { useState } from "react";
 
 const Container = styled.div`
   display: flex;
@@ -33,15 +34,45 @@ const Container = styled.div`
     padding: 5px 10px;
     border-radius: 25px;
   }
+  input {
+    opacity: 0;
+    position: absolute;
+    height: 2.5rem;
+    top: 0;
+    left: 0;
+    width: 44px;
+  }
 `;
 
 const Profile = ({ user }) => {
+  const [imageInput, setImageInput] = useState("");
+  const [previewSource, setPreviewSource] = useState("");
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    previewFile(file);
+    setImageInput(event.target.value);
+  };
+
+  const previewFile = (file) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      setPreviewSource(reader.result);
+    };
+  };
+
   return (
     <Container>
       <h2>{user}</h2>
       <div>
-        <img src={Avatar} alt="avatar" />
+        {previewSource ? (
+          <img src={previewSource} alt="avatar" />
+        ) : (
+          <img src={Avatar} alt="avatar" />
+        )}
         <Button>
+          <input type="file" value={imageInput} onChange={handleImageChange} />
+
           <AddAPhotoIcon />
         </Button>
       </div>
